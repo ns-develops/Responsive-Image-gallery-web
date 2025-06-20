27a602eed7791c6b04194bd2e4666d25661ec63d
”ns-develops” <test.tt9061030@gmail.com>
2025-06-20 17:01:46 +0200

    upd mui, need deprecated check

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,22 +2,16 @@ import React, { useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import './Gallery.css';
 
-import Img1 from './img/img1.jpg';
-import Img2 from './img/img2.jpg';
+
+
 import Img3 from './img/img3.jpg';
-import Img4 from './img/img4.jpg';
 import Img5 from './img/img5.jpg';
-import Img6 from './img/img6.jpg';
 import Img7 from './img/img7.jpg';
 
 const Gallery = () => {
   const data = [
-    { id: 1, imgSrc: Img1 },
-    { id: 2, imgSrc: Img2 },
     { id: 3, imgSrc: Img3 },
-    { id: 4, imgSrc: Img4 },
     { id: 5, imgSrc: Img5 },
-    { id: 6, imgSrc: Img6 },
     { id: 7, imgSrc: Img7 },
   ];
 
